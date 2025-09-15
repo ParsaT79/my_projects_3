95e59fc7397bef8adb439ff34da9a2e3543d808c
parsaT79 <parsa.taraj@gmail.com>
2025-09-15 18:42:26 +0300

    Practice 3 B in the third project, Added.

--- Data_Access/practice_3_b_data_management.py
+++ Data_Access/practice_3_b_data_management.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 def create_table():
-    connection = sqlite3.connect("practice_3_b.db")
+    connection = sqlite3.connect("Data_Access\practice_3_b.db")
     cursor = connection.cursor()
     cursor.execute("""CREATE TABLE IF NOT EXISTS user_list
                       (
